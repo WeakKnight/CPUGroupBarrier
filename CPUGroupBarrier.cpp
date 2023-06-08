e45affac97d1386eb668614ea968d5473315bbc7
Tianyu Li <ltyucb@gmail.com>
2023-06-08 21:27:39 +0800

    Update CPUGroupBarrier.cpp

--- CPUGroupBarrier.cpp
+++ CPUGroupBarrier.cpp
@@ -3,30 +3,27 @@
 #include <thread>
 #include <vector>
 #include <atomic>
+#include <unordered_map>
 
 struct CPUThreadDispatcher
 {
-	CPUThreadDispatcher(uint32_t inWidth, uint32_t inHeight, uint32_t inDepth)
-		: mWidth(inWidth), mHeight(inHeight), mDepth(inDepth)
+	CPUThreadDispatcher()
 	{
-		mAliveThreadCount = inWidth * inHeight * inDepth;
-
 		mCompletionFunction.dispatcher = this;
-		mBarriers.push_back(std::make_shared<std::barrier<CompletionFunction>>(inWidth * inHeight * inDepth, mCompletionFunction));
-		mThreads.reserve((size_t)inWidth * (size_t)inHeight * (size_t)inDepth);
+		mThreads.reserve(256llu);
 	}
 
 	struct GroupContext
 	{
 		GroupContext(CPUThreadDispatcher* dispatcher)
-			:mpDispatcher(dispatcher)
+			: mpDispatcher(dispatcher)
 		{
 		}
 
 		~GroupContext()
 		{
 			mpDispatcher->mAliveThreadCount--;
-			mpDispatcher->mBarriers.back()->arrive_and_drop();
+			mpDispatcher->GetBarrier(mpDispatcher->mCurrentBarrierSize)->arrive_and_drop();
 		}
 
 		CPUThreadDispatcher* mpDispatcher;
@@ -36,21 +33,38 @@ struct CPUThreadDispatcher
 	{
 		void operator()() noexcept
 		{
-			dispatcher->mBarriers.push_back(std::make_shared<std::barrier<CompletionFunction>>(dispatcher->mAliveThreadCount, dispatcher->mCompletionFunction));
+			dispatcher->mCurrentBarrierSize = dispatcher->mAliveThreadCount;
 		}
 
 		CPUThreadDispatcher* dispatcher;
 	};
 
-	void GroupBlock()
+	std::barrier<CompletionFunction>* GetBarrier(uint32_t num)
 	{
-		mBarriers.back()->arrive_and_wait();
+		auto iter = mBarriers.find(num);
+		if (iter == mBarriers.cend())
+		{
+			auto result = std::make_shared<std::barrier<CompletionFunction>>(mAliveThreadCount, mCompletionFunction);
+			mBarriers[num] = result;
+			return result.get();
+		}
+		else
+		{
+			return iter->second.get();
+		}
 	}
 
-	void Dispatch()
+	void GroupBlock()
 	{
-		auto work = [&](uint32_t x, uint32_t y, uint32_t z)
-		{
+		GetBarrier(mCurrentBarrierSize)->arrive_and_wait();
+	}
+
+	void Dispatch(uint32_t blockWidth, uint32_t blockHeight, uint32_t blockDepth)
+	{
+		mCurrentBarrierSize = blockWidth * blockHeight * blockDepth;
+		mAliveThreadCount = mCurrentBarrierSize;
+
+		auto work = [&](uint32_t x, uint32_t y, uint32_t z) {
 			GroupContext groupContext(this);
 
 			printf("Thread %u, %u, %u Initialized. \n", x, y, z);
@@ -69,11 +83,11 @@ struct CPUThreadDispatcher
 			printf("Thread %u, %u, %u Done. \n", x, y, z);
 		};
 
-		for (uint32_t x = 0; x < mWidth; x++)
+		for (uint32_t x = 0; x < blockWidth; x++)
 		{
-			for (uint32_t y = 0; y < mHeight; y++)
+			for (uint32_t y = 0; y < blockHeight; y++)
 			{
-				for (uint32_t z = 0; z < mDepth; z++)
+				for (uint32_t z = 0; z < blockDepth; z++)
 				{
 					mThreads.emplace_back(work, x, y, z);
 				}
@@ -81,10 +95,8 @@ struct CPUThreadDispatcher
 		}
 	}
 
-	uint32_t mWidth;
-	uint32_t mHeight;
-	uint32_t mDepth;
-	std::vector<std::shared_ptr<std::barrier<CompletionFunction>>> mBarriers;
+	uint32_t mCurrentBarrierSize;
+	std::unordered_map<uint32_t, std::shared_ptr<std::barrier<CompletionFunction>>> mBarriers;
 	CompletionFunction mCompletionFunction;
 	std::atomic_uint32_t mAliveThreadCount;
 	std::vector<std::jthread> mThreads;
@@ -96,8 +108,8 @@ struct CPUCoroutineDispatcher
 
 int main()
 {
-	CPUThreadDispatcher dispatcher(8, 1, 1);
-	dispatcher.Dispatch();
+	CPUThreadDispatcher dispatcher;
+	dispatcher.Dispatch(8, 1, 1);
 
 	return 0;
 }
