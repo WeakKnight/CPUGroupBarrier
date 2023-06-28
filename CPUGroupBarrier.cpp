016e081bab16088bca973b04baece26d559b4f82
Tianyu Li <ltyucb@gmail.com>
2023-06-28 20:02:04 +0800

    Update CPUGroupBarrier.cpp

--- CPUGroupBarrier.cpp
+++ CPUGroupBarrier.cpp
@@ -5,6 +5,7 @@
 #include <atomic>
 #include <unordered_map>
 #include <coroutine>
+#include <optional>
 
 #include <windows.h>
 
@@ -12,256 +13,115 @@
 #define CONCAT_IMPL(a, b) a##b
 #define CONCAT(a, b) CONCAT_IMPL(a, b)
 
-struct ThreadDispatcher
+struct CPUDispatcher
 {
-	ThreadDispatcher()
+	CPUDispatcher()
+		: mCompletionFunction(CompletionFunction()), mBarrierRed(std::barrier<CompletionFunction>(BLOCK_WIDTH, mCompletionFunction)), mBarrierBlack(std::barrier<CompletionFunction>(BLOCK_WIDTH, mCompletionFunction))
 	{
-		mCurrentBarrierSize = 0;
-		mCompletionFunction.dispatcher = this;
-		mThreads.reserve(BLOCK_WIDTH);
+		mThreadGroupRed.reserve(BLOCK_WIDTH);
+		mThreadGroupBlack.reserve(BLOCK_WIDTH);
 	}
-
-	struct GroupContext
-	{
-		GroupContext(ThreadDispatcher* dispatcher)
-			: mpDispatcher(dispatcher)
-		{
-		}
-
-		~GroupContext()
-		{
-			mpDispatcher->mAliveThreadCount--;
-			mpDispatcher->GetBarrier(mpDispatcher->mCurrentBarrierSize)->arrive_and_drop();
-		}
-
-		ThreadDispatcher* mpDispatcher;
-	};
 
 	struct CompletionFunction
 	{
+		CompletionFunction()
+		{
+		}
+
 		void operator()() noexcept
 		{
-			dispatcher->mCurrentBarrierSize = dispatcher->mAliveThreadCount;
 		}
-
-		ThreadDispatcher* dispatcher;
 	};
 
-	std::barrier<CompletionFunction>* GetBarrier(uint32_t num)
+	void Dispatch(uint32_t groupWidth)
 	{
-		auto iter = mBarriers.find(num);
-		if (iter == mBarriers.cend())
+		for (int i = 0; i < groupWidth; i++)
 		{
-			auto result = std::make_shared<std::barrier<CompletionFunction>>(mAliveThreadCount, mCompletionFunction);
-			mBarriers[num] = result;
-			return result.get();
+			DispatchGroup(i);
+			mUseRed = !mUseRed;
 		}
-		else
+
+		for (uint32_t x = 0; x < mThreadGroupRed.size(); x++)
 		{
-			return iter->second.get();
+			mThreadGroupRed[x].join();
 		}
+		mThreadGroupRed.clear();
+
+		for (uint32_t x = 0; x < mThreadGroupBlack.size(); x++)
+		{
+			mThreadGroupBlack[x].join();
+		}
+		mThreadGroupBlack.clear();
 	}
 
-	void GroupBlock()
+	void DispatchGroup(uint32_t gIndex)
 	{
-		GetBarrier(mCurrentBarrierSize)->arrive_and_wait();
-	}
+		auto work = [&](uint32_t groupIndex, uint32_t threadIndex, bool useRed) {
+			printf("[ThreadDispatcher] Group %u Thread %u Initialized. \n", groupIndex, threadIndex);
 
-	void Dispatch()
-	{
-		mCurrentBarrierSize = BLOCK_WIDTH;
-		mAliveThreadCount = mCurrentBarrierSize;
+			if (useRed)
+			{
+				mBarrierRed.arrive_and_wait();
+			}
+			else
+			{
+				mBarrierBlack.arrive_and_wait();
+			}
 
-		auto work = [&](uint32_t threadIndex) {
-			GroupContext groupContext(this);
+			printf("[ThreadDispatcher] Group %u Thread %u Task A Done. \n", groupIndex, threadIndex);
 
-			printf("[ThreadDispatcher] Thread %u Initialized. \n", threadIndex);
+			if (useRed)
+			{
+				mBarrierRed.arrive_and_wait();
+			}
+			else
+			{
+				mBarrierBlack.arrive_and_wait();
+			}
 
-			GroupBlock();
+			printf("[ThreadDispatcher] Group %u Thread %u Task B Done. \n", groupIndex, threadIndex);
 
-			printf("[ThreadDispatcher] Thread %u Task A Done. \n", threadIndex);
+			if (useRed)
+			{
+				mBarrierRed.arrive_and_wait();
+			}
+			else
+			{
+				mBarrierBlack.arrive_and_wait();
+			}
 
-			GroupBlock();
-
-			printf("[ThreadDispatcher] Thread %u Task B Done. \n", threadIndex);
-
-			GroupBlock();
-
-			printf("[ThreadDispatcher] Thread %u Finalized. \n", threadIndex);
+			printf("[ThreadDispatcher] Group %u Thread %u Finalized. \n", groupIndex, threadIndex);
 		};
+
+		std::vector<std::thread>& currentThreadGroup = mUseRed ? mThreadGroupRed : mThreadGroupBlack;
+		std::vector<std::thread>& otherThreadGroup = mUseRed ? mThreadGroupBlack : mThreadGroupRed;
 
 		for (uint32_t x = 0; x < BLOCK_WIDTH; x++)
 		{
-			mThreads.emplace_back(work, x);
+			currentThreadGroup.emplace_back(work, gIndex, x, mUseRed);
 		}
+
+		for (uint32_t x = 0; x < otherThreadGroup.size(); x++)
+		{
+			otherThreadGroup[x].join();
+		}
+		otherThreadGroup.clear();
 	}
 
-	uint32_t mCurrentBarrierSize;
-	std::unordered_map<uint32_t, std::shared_ptr<std::barrier<CompletionFunction>>> mBarriers;
 	CompletionFunction mCompletionFunction;
-	std::atomic_uint32_t mAliveThreadCount;
-	std::vector<std::jthread> mThreads;
+
+	std::barrier<CompletionFunction> mBarrierRed;
+	std::barrier<CompletionFunction> mBarrierBlack;
+
+	bool mUseRed = true;
+	std::vector<std::thread> mThreadGroupRed;
+	std::vector<std::thread> mThreadGroupBlack;
 };
-
-#define KERNEL_BEGIN                                                          \
-	std::unordered_map<uint16_t, uint16_t> completeThreadNums;                \
-	auto IncrementCompleteThreadNum = [&completeThreadNums](int lineNumber) { \
-		auto iter = completeThreadNums.find(lineNumber);                      \
-		if (iter == completeThreadNums.cend())                                \
-		{                                                                     \
-			completeThreadNums[lineNumber] = 1;                               \
-			return uint16_t(1);                                               \
-		}                                                                     \
-		else                                                                  \
-		{                                                                     \
-			iter->second++;                                                   \
-			return iter->second;                                              \
-		}                                                                     \
-	};                                                                        \
-	int state = 0;                                                            \
-	for (uint32_t threadIndex = 0; threadIndex < BLOCK_WIDTH; threadIndex++)  \
-	{                                                                         \
-		switch (state)                                                        \
-		{                                                                     \
-			case 0:
-
-#define GROUP_BARRIER                                        \
-	if (IncrementCompleteThreadNum(__LINE__) != BLOCK_WIDTH) \
-	{                                                        \
-		continue;                                            \
-	}                                                        \
-	else                                                     \
-	{                                                        \
-		threadIndex = 0;                                     \
-		state = __LINE__;                                    \
-	}                                                        \
-	case __LINE__:
-
-#define KERNEL_END \
-	}              \
-	}
-
-struct CoroutineDispatcher
-{
-	void Dispatch()
-	{
-		KERNEL_BEGIN
-
-		printf("[CoroutineDispatcher] Thread %u Initialized. \n", threadIndex);
-
-		GROUP_BARRIER
-
-		printf("[CoroutineDispatcher] Thread %u Task A Done. \n", threadIndex);
-
-		GROUP_BARRIER
-
-		printf("[CoroutineDispatcher] Thread %u Task B Done. \n", threadIndex);
-
-		GROUP_BARRIER
-
-		printf("[CoroutineDispatcher] Thread %u Finalized. \n", threadIndex);
-
-		KERNEL_END
-	}
-};
-
-struct resumable_thing
-{
-	struct promise_type
-	{
-		resumable_thing get_return_object()
-		{
-			return resumable_thing(std::coroutine_handle<promise_type>::from_promise(*this));
-		}
-		auto initial_suspend() { return std::suspend_never{}; }
-		auto final_suspend() noexcept { return std::suspend_never{}; }
-		void return_void() {}
-
-		void unhandled_exception() {}
-	};
-	std::coroutine_handle<promise_type> _coroutine = nullptr;
-	resumable_thing() = default;
-	resumable_thing(resumable_thing const&) = delete;
-	resumable_thing& operator=(resumable_thing const&) = delete;
-	resumable_thing(resumable_thing&& other)
-		: _coroutine(other._coroutine)
-	{
-		other._coroutine = nullptr;
-	}
-	resumable_thing& operator=(resumable_thing&& other)
-	{
-		if (&other != this)
-		{
-			_coroutine = other._coroutine;
-			other._coroutine = nullptr;
-		}
-	}
-	explicit resumable_thing(std::coroutine_handle<promise_type> coroutine)
-		: _coroutine(coroutine)
-	{
-	}
-	~resumable_thing()
-	{
-		if (_coroutine && _coroutine.done())
-		{
-			_coroutine.destroy();
-		}
-	}
-	void resume()
-	{
-		if (!_coroutine.done())
-		{
-			_coroutine.resume();
-		}
-	}
-
-	bool is_done() const { return _coroutine.done(); }
-};
-
-bool sHasRemainingResumable = true;
-
-resumable_thing counter(int i)
-{
-	// std::unordered_map<uint16_t, uint16_t> completeThreadNums;
-	// auto IncrementCompleteThreadNum = [&completeThreadNums](int lineNumber) {
-	//	auto iter = completeThreadNums.find(lineNumber);
-	//	if (iter == completeThreadNums.cend())
-	//	{
-	//		completeThreadNums[lineNumber] = 1;
-	//		return uint16_t(1);
-	//	}
-	//	else
-	//	{
-	//		iter->second++;
-	//		return iter->second;
-	//	}
-	// };
-
-	printf("[ThreadDispatcher] Thread %u Initialized. \n", i);
-
-	co_await std::suspend_always{};
-
-	printf("[ThreadDispatcher] Thread %u Task A Done. \n", i);
-
-	co_await std::suspend_always{};
-
-	printf("[ThreadDispatcher] Thread %u Finalized. \n", i);
-
-	sHasRemainingResumable = false;
-	co_return;
-}
 
 int main()
 {
-	resumable_thing the_counter = counter(0);
-
-	while (sHasRemainingResumable)
-	{
-		if (!the_counter._coroutine.done())
-		{
-			the_counter.resume();
-		}
-	}
+	CPUDispatcher dispatcher;
+	dispatcher.Dispatch(4);
 
 	return 0;
 }
